d1ebe1091c8f8acb3a7fc01a1cc624ac5092db31
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2021-12-28 20:08:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ console.log("Hello World");
 let name = 'Dennis';
 console.log(name);
 
+// Rules for variables
+// cannot be a reserved keyword
+// Should be meaningful
+//Cannot start with a number (1name)
+// cannot contain a space or hyphen (-)
+//Are case-sensitive
+
+let firstName = 'Dennis'; //string literal
+let lastName = 'Nyarko'; //string literal
+let age = 99; //number literal
+let isApproved = false; // boolean literal
+let middleName = undefined;
+let selectedColor = null;
+
+//use let when variable can be changed along the line
+//use const when variable is constant (changing it anywhere in th code causes and error)    
