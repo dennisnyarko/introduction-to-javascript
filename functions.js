444fe0801c8b5c0c7b022c6223b7508cb2507224
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2021-12-28 20:19:43 +0000

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,12 @@
+let person = {
+  name: "Dennis',
+  age: 30
+};
+
+//displaying results person function
+console.log(person);
+
+//Another method
+//Dot Notation
+person.name = 'Kwasi';
+console.log(person.name);
