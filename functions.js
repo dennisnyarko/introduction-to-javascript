4ee0dabd1917d46e0b96b02f8de77f51e0d28590
Dennis Nyarko <96777791+dennisnyarko@users.noreply.github.com>
2021-12-29 00:06:20 +0000

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,26 @@
+// Performing a task
+function greet() {
+ console.log('Good Morning');   
+}
+
+greet();
+
+
+//2nd example
+// used a different function and variable name
+function welcome(fname) {
+    console.log('Good Morning ' + fname);   
+   }
+   
+   welcome('Dennis');
+
+   //treat codes as separate
+   //comment out unwanted lines
+
+   // Calculating a value
+   function square(number) {
+       return number * number;
+   }
+
+   let number = square(2);
+   console.log(number); // you can do this straight without declaring a variable
